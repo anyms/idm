2fa96e9ef4a8daaa9f1cbda78d7bf5319d440275
anyms <mytellee@gmail.com>
2020-04-30 20:49:54 +0530

    bug fixes

--- idm/src/main/java/app/spidy/idm/FileIO.kt
+++ idm/src/main/java/app/spidy/idm/FileIO.kt
@@ -9,74 +9,77 @@ import android.util.Log
 import app.spidy.idm.interfaces.CopyListener
 import java.io.*
 import java.lang.Exception
+import kotlin.concurrent.thread
 
 
 class FileIO(private val context: Context) {
     fun copyToSdCard(file: File, sdCardLocation: String, mimeType: String = "*/*", copyListener: CopyListener? = null) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val values = ContentValues()
-            values.put(MediaStore.Images.Media.TITLE, file.name)
-            values.put(MediaStore.Images.Media.DISPLAY_NAME, file.name)
-            values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
-            values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
-            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
-            values.put(MediaStore.Images.Media.RELATIVE_PATH, sdCardLocation)
-            val uri = context.contentResolver.insert(MediaStore.Files.getContentUri("external"), values)
-            val descriptor =
-                context.contentResolver.openFileDescriptor(uri!!, "w") //"w" specify's write mode
-            val fileDescriptor: FileDescriptor = descriptor!!.fileDescriptor
-            val dataInputStream: InputStream = FileInputStream(file.absolutePath)
+        thread {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.TITLE, file.name)
+                values.put(MediaStore.Images.Media.DISPLAY_NAME, file.name)
+                values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+                values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+                values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                values.put(MediaStore.Images.Media.RELATIVE_PATH, sdCardLocation)
+                val uri = context.contentResolver.insert(MediaStore.Files.getContentUri("external"), values)
+                val descriptor =
+                    context.contentResolver.openFileDescriptor(uri!!, "w") //"w" specify's write mode
+                val fileDescriptor: FileDescriptor = descriptor!!.fileDescriptor
+                val dataInputStream: InputStream = FileInputStream(file.absolutePath)
 
-            val output = FileOutputStream(fileDescriptor)
-            val buf = ByteArray(4096)
-            var bytesRead = dataInputStream.read(buf)
-            var copiedBytes = bytesRead.toLong()
-            var lastProgress = 0
-            while (bytesRead > 0) {
-                output.write(buf, 0, bytesRead)
-                copiedBytes += bytesRead
-                listenProgress(copiedBytes, file.length()) {
-                    lastProgress = it
-                    copyListener?.onCopy(it)
-                }
-                bytesRead = dataInputStream.read(buf)
-            }
-            if (lastProgress != 100) {
-                copyListener?.onCopy(100)
-            }
-            dataInputStream.close()
-            output.close()
-            copyListener?.onCopied()
-        } else {
-            val dir = Environment.getExternalStoragePublicDirectory(sdCardLocation).absolutePath
-            val destination = RandomAccessFile("$dir${File.separator}${file.name}", "rw")
-            destination.seek(0)
-
-            var fis: FileInputStream? = null
-            try {
-                fis = FileInputStream(file)
-                val bytes = ByteArray(4096)
-                var bytesRead = fis.read(bytes)
+                val output = FileOutputStream(fileDescriptor)
+                val buf = ByteArray(4096)
+                var bytesRead = dataInputStream.read(buf)
                 var copiedBytes = bytesRead.toLong()
                 var lastProgress = 0
-                while (bytesRead != -1) {
-                    destination.write(bytes, 0, bytesRead)
+                while (bytesRead > 0) {
+                    output.write(buf, 0, bytesRead)
                     copiedBytes += bytesRead
                     listenProgress(copiedBytes, file.length()) {
                         lastProgress = it
                         copyListener?.onCopy(it)
                     }
-                    bytesRead = fis.read(bytes)
+                    bytesRead = dataInputStream.read(buf)
                 }
                 if (lastProgress != 100) {
                     copyListener?.onCopy(100)
                 }
+                dataInputStream.close()
+                output.close()
                 copyListener?.onCopied()
-            } catch (e: Exception) {
-                copyListener?.onCopyError(e)
-            } finally {
-                destination.close()
-                fis?.close()
+            } else {
+                val dir = Environment.getExternalStoragePublicDirectory(sdCardLocation).absolutePath
+                val destination = RandomAccessFile("$dir${File.separator}${file.name}", "rw")
+                destination.seek(0)
+
+                var fis: FileInputStream? = null
+                try {
+                    fis = FileInputStream(file)
+                    val bytes = ByteArray(4096)
+                    var bytesRead = fis.read(bytes)
+                    var copiedBytes = bytesRead.toLong()
+                    var lastProgress = 0
+                    while (bytesRead != -1) {
+                        destination.write(bytes, 0, bytesRead)
+                        copiedBytes += bytesRead
+                        listenProgress(copiedBytes, file.length()) {
+                            lastProgress = it
+                            copyListener?.onCopy(it)
+                        }
+                        bytesRead = fis.read(bytes)
+                    }
+                    if (lastProgress != 100) {
+                        copyListener?.onCopy(100)
+                    }
+                    copyListener?.onCopied()
+                } catch (e: Exception) {
+                    copyListener?.onCopyError(e)
+                } finally {
+                    destination.close()
+                    fis?.close()
+                }
             }
         }
     }
